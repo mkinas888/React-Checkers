b9d5fbc5e685753cc7f15bde3587ae4ccba75616
mkinas888 <mkinas888@gmail.com>
2018-05-24 21:22:09 +0200

    Game board becomes active

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
+
+    handleOnClick = (e) => {
+        this.props.activatePiece(e.target.value);
+    }
+
     render () {
         return (
-            <div className={'cell-'+this.props.cellColor}>
-                <button className={`gamePiece-${this.props.belongsTo}`}/>
+            <div className={`cell-${this.props.cellColor}-${this.props.isActive}`}>
+                <button className={`gamePiece-${this.props.belongsTo}`} 
+                        onClick={this.handleOnClick} value={this.props.cellId}/>
             </div>
         );
     }
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,47 +1,70 @@
 import React, { Component } from 'react';
 import Cell from './Cell';
 import './GameBoard.css';
+import data from './BoardData'
 
 class GameBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: [
-        ['b','-','b','-','b','-','b','-'],
-        ['-','b','-','b','-','b','-','b'],
-        ['b','-','b','-','b','-','b','-'],
-        ['-','c','-','c','-','c','-','c'],
-        ['c','-','c','-','c','-','c','-'],
-        ['-','r','-','r','-','r','-','r'],
-        ['r','-','r','-','r','-','r','-'],
-        ['-','r','-','r','-','r','-','r']],
-      activePlayer: 'red'
+      board: data,
+      activePlayer: 'red',
     };
   }
 
-  whichCell (v, index) {
-    console.log(index);
-    if(v == 'b') {
-      return <Cell cellColor={'black'} belongsTo={'black'}/>
-    } else if(v == 'r') {
-      return <Cell cellColor={'black'} belongsTo={'red'}/>
-    } else {      
-      if(v === 'c') {
-        return <Cell cellColor={'black'} belongsTo={'none'}/>
+  whichCell = (v) => {
+    var isBlack = v.color === 'black' ? true : false;
+    if(v.player === 'b') {
+      if(isBlack) {
+        return <Cell cellColor={'black'} belongsTo={'black'} 
+                     activatePiece={this.activatePiece} cellId={v.id} isActive={v.active}/>
       } else {
-        return <Cell cellColor={'white'} belongsTo={'none'}/>
+        return <Cell cellColor={'white'} belongsTo={'black'} 
+                     activatePiece={this.activatePiece} cellId={v.id} isActive={v.active}/>
+      }
+    } else if(v.player === 'r') {
+      if(isBlack) {
+        return <Cell cellColor={'black'} belongsTo={'red'} 
+                     activatePiece={this.activatePiece} cellId={v.id} isActive={v.active}/>
+      } else {
+        return <Cell cellColor={'white'} belongsTo={'red'} 
+                     activatePiece={this.activatePiece} cellId={v.id} isActive={v.active}/>
+      }
+    } else {
+      if(isBlack) {
+        return <Cell cellColor={'black'} belongsTo={'none'} 
+                     activatePiece={this.activatePiece} cellId={v.id} isActive={v.active}/>
+      } else {
+        return <Cell cellColor={'white'} belongsTo={'none'} 
+                     activatePiece={this.activatePiece} cellId={v.id} isActive={v.active}/>
       }
     }
+  }
+
+  renderCells = () => {
+    return (
+      <div className="GameBoard">
+        {this.state.board.map(row => row.map(this.whichCell))}
+      </div>
+    );
+  } 
+
+  activatePiece = (value) => {
+    var id1 = Math.floor(value/10) - 1;
+    var id2 = value % 10 - 1;
+    var tmpObject = [...this.state.board]
+    tmpObject.map(row => row.map(cell => cell.active = false));
+    tmpObject[id1][id2].active = true;
+    this.setState({board: tmpObject});
   }
 
   
 
   render() {
+    console.log(this.state.board[1][2]);
     return (
       <div className="GameBoard-container">
-        <div className="GameBoard">
-          {this.state.board.map(row => row.map(this.whichCell))}
-        </div>
+        {this.renderCells()}
       </div>
     );
   }
