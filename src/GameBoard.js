9e116709e8cd90d5280e5b6977e5dae0ac11525b
mkinas888 <mkinas888@gmail.com>
2018-05-28 20:57:47 +0200

    Add beating detection

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -10,7 +10,9 @@ class GameBoard extends Component {
       board: data,
       activePlayer: 'r',
       previousPiecePosition: [],
-      possibleMovesIds: [] 
+      possibleMovesIds: [],
+      availableBeatingsUp: [],
+      availableBeatingsDown: [] 
     };
   }
 
@@ -52,46 +54,49 @@ class GameBoard extends Component {
     );
   } 
 
-  setPossibleMoves = (id1, id2, player, board) => {
-    var a = [];
-    this.setState({possibleMovesIds: a});
-    this.setState({previousPiecePosition: a});
+  setPossibleMoves = (id1, id2, player) => {
+    var empty = [];
+    this.setState({possibleMovesIds: empty});
+    this.setState({previousPiecePosition: empty});
     // remove active state from previously activated 
-    board.map(row => row.map(cell => cell.active = false));
-    if(board[id1][id2].player !== 'none'){
+    this.state.board.map(row => row.map(cell => cell.active = false));
+    if(this.state.activePlayer !== this.state.board[id1][id2].player){
+      return;
+    }
+    if(this.state.board[id1][id2].player !== 'none'){
       if(player === 'r') {
-        if(board[id1][id2].player === 'r') {
-          board[id1][id2].active = true;
+        if(this.state.board[id1][id2].player === 'r') {
+          this.state.board[id1][id2].active = true;
           this.setState(prevState => ({
-            previousPiecePosition: [...prevState.previousPiecePosition, id1, id2]
+            previousPiecePosition: [...prevState.previousPiecePosition, id1, id2],
           })); 
         }
         if(id1 > 0){
           if(id2 > 0 && id2 < 7){
-            if(board[id1-1][id2-1].player === 'none') {
-              board[id1-1][id2-1].active = true;
+            if(this.state.board[id1-1][id2-1].player === 'none') {
+              this.state.board[id1-1][id2-1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1-1, id2-1]
               }));
             } 
-            if (board[id1-1][id2+1].player === 'none') {
-              board[id1-1][id2+1].active = true;
+            if (this.state.board[id1-1][id2+1].player === 'none') {
+              this.state.board[id1-1][id2+1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1-1, id2+1]
               }));
             }
           }
           if(id2 === 0) {
-            if (board[id1-1][id2+1].player === 'none') {
-              board[id1-1][id2+1].active = true;
+            if (this.state.board[id1-1][id2+1].player === 'none') {
+              this.state.board[id1-1][id2+1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1-1, id2+1]
               }));
             }
           }
           if(id2 === 7) {
-            if(board[id1-1][id2-1].player === 'none') {
-              board[id1-1][id2-1].active = true;
+            if(this.state.board[id1-1][id2-1].player === 'none') {
+              this.state.board[id1-1][id2-1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1-1, id2-1]
               }));
@@ -99,38 +104,38 @@ class GameBoard extends Component {
           }
         }
       } else if(player === 'b') {
-        if(board[id1][id2].player === 'b') {
-          board[id1][id2].active = true;
+        if(this.state.board[id1][id2].player === 'b') {
+          this.state.board[id1][id2].active = true;
           this.setState(prevState => ({
             previousPiecePosition: [...prevState.previousPiecePosition, id1, id2]
           })); 
         }
         if(id1 < 7){
           if(id2 > 0 && id2 < 7){
-            if(board[id1+1][id2-1].player === 'none') {
-              board[id1+1][id2-1].active = true;
+            if(this.state.board[id1+1][id2-1].player === 'none') {
+              this.state.board[id1+1][id2-1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1+1, id2-1]
               }));
             } 
-            if (board[id1+1][id2+1].player === 'none') {
-              board[id1+1][id2+1].active = true;
+            if (this.state.board[id1+1][id2+1].player === 'none') {
+              this.state.board[id1+1][id2+1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1+1, id2+1]
               }));
             }
           }
           if(id2 === 0) {
-            if (board[id1+1][id2+1].player === 'none') {
-              board[id1+1][id2+1].active = true;
+            if (this.state.board[id1+1][id2+1].player === 'none') {
+              this.state.board[id1+1][id2+1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1+1, id2+1]
               }));
             }
           }
           if(id2 === 7) {
-            if(board[id1+1][id2-1].player === 'none') {
-              board[id1+1][id2-1].active = true;
+            if(this.state.board[id1+1][id2-1].player === 'none') {
+              this.state.board[id1+1][id2-1].active = true;
               this.setState(prevState => ({
                 possibleMovesIds: [...prevState.possibleMovesIds, id1+1, id2-1]
               }));
@@ -139,29 +144,209 @@ class GameBoard extends Component {
         }
       }
     }
-    return board;
+  }
+
+  checkIfBeatingUpAvailable = (id1, id2) => {
+    var empty = [];
+    this.setState({availableBeatingsUp: empty});
+    if(this.state.activePlayer === 'r'){
+      if(id1 < 6) {
+        if(id2 < 2) {
+          if(this.state.board[id1+1][id2+1].player === 'b') {
+            if(this.state.board[id1+2][id2+2].player === 'none') {
+              this.state.board[id1+2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1+2, id2+2]
+              }));
+            }
+          }
+        }
+        if(id2 > 5){
+          if(this.state.board[id1+1][id2-1].player === 'b') {
+            if(this.state.board[id1+2][id2-2].player === 'none') {
+              this.state.board[id1+2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1+2, id2-2]
+              }));
+            }
+          }
+        }
+        if(id2 >= 2 && id2 <= 5) {
+          if(this.state.board[id1+1][id2+1].player === 'b') {
+            if(this.state.board[id1+2][id2+2].player === 'none') {
+              this.state.board[id1+2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1+2, id2+2]
+              }));
+            }
+          }
+          if(this.state.board[id1+1][id2-1].player === 'b') {
+            if(this.state.board[id1+2][id2-2].player === 'none') {
+              this.state.board[id1+2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1+2, id2-2]
+              }));
+            }
+          }
+        }
+      }
+    }
+    else {
+      if(id1 > 1) {
+        if(id2 < 2) {
+          if(this.state.board[id1-1][id2+1].player === 'r') {
+            if(this.state.board[id1-2][id2+2].player === 'none') {
+              this.state.board[id1-2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1-2, id2+2]
+              }));
+            }
+          }
+        }
+        if(id2 > 5){
+          if(this.state.board[id1-1][id2-1].player === 'r') {
+            if(this.state.board[id1-2][id2-2].player === 'none') {
+              this.state.board[id1-2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1-2, id2-2]
+              }));
+            }
+          }
+        }
+        if (id2 >= 2 && id2 <= 5) {
+          if(this.state.board[id1-1][id2+1].player === 'r') {
+            if(this.state.board[id1-2][id2+2].player === 'none') {
+              this.state.board[id1-2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1-2, id2+2]
+              }));
+            }
+          }
+          if(this.state.board[id1-1][id2-1].player === 'r') {
+            if(this.state.board[id1-2][id2-2].player === 'none') {
+              this.state.board[id1-2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsUp: [...prevState.availableBeatingsUp, id1-2, id2-2]
+              }));
+            }
+          }
+        }
+      }
+    }
+  }
+
+  checkIfBeatingDownAvailable = (id1, id2) => {
+    var empty = [];
+    this.setState({availableBeatingsDown: empty});
+    if(this.state.activePlayer === 'r'){
+      if(id1 > 1) {
+        if(id2 < 2) {
+          if(this.state.board[id1-1][id2+1].player === 'b') {
+            if(this.state.board[id1-2][id2+2].player === 'none') {
+              this.state.board[id1-2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1-2, id2+2]
+              }));
+            }
+          }
+        }
+        if(id2 > 5){
+          if(this.state.board[id1-1][id2-1].player === 'b') {
+            if(this.state.board[id1-2][id2-2].player === 'none') {
+              this.state.board[id1-2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1-2, id2-2]
+              }));
+            }
+          }
+        }
+        if(id2 >= 2 && id2 <= 5) {
+          if(this.state.board[id1-1][id2+1].player === 'b') {
+            if(this.state.board[id1-2][id2+2].player === 'none') {
+              this.state.board[id1-2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1-2, id2+2]
+              }));
+            }
+          }
+          if(this.state.board[id1-1][id2-1].player === 'b') {
+            if(this.state.board[id1-2][id2-2].player === 'none') {
+              this.state.board[id1-2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1-2, id2-2]
+              }));
+            }
+          }
+        }
+      }
+    }
+    else {
+      if(id1 < 6) {
+        if(id2 < 2) {
+          if(this.state.board[id1+1][id2+1].player === 'r') {
+            if(this.state.board[id1+2][id2+2].player === 'none') {
+              this.state.board[id1+2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1+2, id2+2]
+              }));
+            }
+          }
+        }
+        if(id2 > 5){
+          if(this.state.board[id1+1][id2-1].player === 'r') {
+            if(this.state.board[id1+2][id2-2].player === 'none') {
+              this.state.board[id1+2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1+2, id2-2]
+              }));
+            }
+          }
+        }
+        if (id2 >= 2 && id2 <= 5) {
+          if(this.state.board[id1+1][id2+1].player === 'r') {
+            if(this.state.board[id1+2][id2+2].player === 'none') {
+              this.state.board[id1+2][id2+2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1+2, id2+2]
+              }));
+            }
+          }
+          if(this.state.board[id1+1][id2-1].player === 'r') {
+            if(this.state.board[id1+2][id2-2].player === 'none') {
+              this.state.board[id1+2][id2-2].active = true;
+              this.setState(prevState => ({
+                availableBeatingsDown: [...prevState.availableBeatingsDown, id1+2, id2-2]
+              }));
+            }
+          }
+        }
+      }
+    }
   }
 
   activatePiece = (value) => {
     //get id of first and second table from clicked button's id
     var id1 = Math.floor(value/10) - 1;
     var id2 = value % 10 - 1;
-    this.setPossibleMoves(id1, id2, this.state.activePlayer, this.state.board);
-    if( this.state.board[id1][id2].player === 'none' && this.state.possibleMovesIds.length !== 0){
+    this.setPossibleMoves(id1, id2, this.state.activePlayer);
+    this.checkIfBeatingUpAvailable(id1, id2);
+    this.checkIfBeatingDownAvailable(id1, id2);
+    if( this.state.board[id1][id2].player === 'none' && 
+        this.state.possibleMovesIds.length !== 0 && this.state.board[id1][id2].color === 'black'){
       this.performMove(id1, id2);
       return;
     }
   }
 
   performMove = (id1, id2) => {
-    var a = [];
+    var empty = [];
     var tmpBoard = [...this.state.board];
     tmpBoard[id1][id2].player = this.state.activePlayer;
     tmpBoard[this.state.previousPiecePosition[0]][this.state.previousPiecePosition[1]].player = 'none';
     this.setState({
       board: tmpBoard,
       activePlayer: this.state.activePlayer === 'r' ? 'b' : 'r',
-      previousPiecePosition: a
+      previousPiecePosition: empty
     });
     this.renderCells;
   }
